ec7294f3c63bf5ee6da21133ca7b387a15ce96bc
nalini793 <97171653+nalini793@users.noreply.github.com>
2022-06-27 19:00:07 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,9 +6,10 @@ export default function Welcome(props){
        {name: props.name}
     );
     return(
-        <div>
-    <h1>Hey !{props.name}</h1>
-    <h2>Welcome to Newton School.</h2>
-    </div>
+      <div>
+        <h1>Hey !<h1>{props.name}</h1>
+            </h1>
+        <h2>Welcome to Newton School.</h2>
+        </div>
     )
 }
