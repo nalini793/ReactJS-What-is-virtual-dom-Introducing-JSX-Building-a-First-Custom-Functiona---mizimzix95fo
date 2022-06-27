3133564218f79be9a43393354a01316680c4bea1
nalini793 <97171653+nalini793@users.noreply.github.com>
2022-06-27 18:37:19 +0530

    use data and dataset

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,13 @@
 // write code for Welcome component here
-import React from "react";
+import React, {Component, useState} from "react";
 
 export default function Welcome(props){
+    const [data, setData] = useState(
+       {name: props.name}
+    );
     return(
         <div>
-    <h1>Hey ! {" "+props.name}</h1>
+    <h1>Hey ! {props.name}</h1>
     <h2>Welcome to Newton School.</h2>
     </div>
     )
